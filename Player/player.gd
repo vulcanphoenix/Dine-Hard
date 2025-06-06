c739ddf2d521031318a04ec2be8ce3d279d2729d
vulcanphoenix <vilste20@kingshigh.school.nz>
2025-06-06 12:25:47 +1200

    Resturant

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 var speed = 400
 func _physics_process(delta):
-	var direction = Input.get_vector("down", "left", "right", "up")
+	var direction = Input.get_vector( "left", "right", "up", "down")
 	velocity = direction * speed
 	
 	move_and_slide()
